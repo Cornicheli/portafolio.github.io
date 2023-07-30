5ebe4ba3d9d92aa8053e4009fdaa6cc317796c72
cornicheli <gabrielcornide.pg@gmail.com>
2023-07-30 15:16:57 -0300

    cambios de cartas en proyecto

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,15 +1,29 @@
 import BgAboutMe from "./bg-about.webp";
 import LogoContact from "./icon-contact.png";
 import Favicon from "./favicon-porfolio.ico";
-import GameCenter from "./GameCenter.png";
-import GameCenterNative from "./GameCenterNative.png";
 import MyPorfolioElectron from "./MyPorfolioElectron.png";
+
+// MyTinerary
 import MyTinerary from "./MyTinerary.png";
 import MyTineraryNative from "./MyTineraryNative.png";
-import Touken from "./touken.png";
+
+// GameCenter
+import GameCenter from "./GameCenter.png";
+import GameCenterNative from "./GameCenterNative.png";
+
+
+// Touken
+import Touken1 from "./touken1.png";
+import Touken2 from "./touken2.png";
+import Touken3 from "./touken3.png";
+import Touken4 from "./touken4.png";
+import Touken5 from "./touken5.png";
+import Touken6 from "./touken6.png";
+import Touken7 from "./touken7.png";
 import ToukenMobile from "./touken-mobile.jpg";
 
 export {
+  Touken1, Touken2, Touken3, Touken4, Touken5, Touken6, Touken7,
   BgAboutMe,
   Favicon,
   LogoContact,
@@ -18,6 +32,5 @@ export {
   MyPorfolioElectron,
   MyTinerary,
   MyTineraryNative,
-  Touken,
   ToukenMobile,
 };
--- src/components/myproyect/MyProyect.jsx
+++ src/components/myproyect/MyProyect.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import Card from "../card/Card";
 import "./myProyect.css";
+import {
+  GameCenter,
+  MyTinerary,
+  GameCenterNative,
+  MyTineraryNative,
+  MyPorfolioElectron,
+  Touken1, Touken2, Touken3, Touken4, Touken5, Touken6, Touken7,
+  ToukenMobile,
+} from "../../assets/img/index";
 
 export default function MyProyect() {
   return (
     <>
       <div className="ctn-img-myproyect" id="proyect">
-        <Card />
+        {/* <Card /> */}
+        <Card
+          imgCard={Touken1}
+          title="→ Touken"
+          text='Bienvenido a la web de pre-lanzamiento de StartUp Chile. ¡Nuestra app móvil! En esta plataforma, tanto usuarios como empresas pueden inscribirse para el próximo lanzamiento. Simplemente, elige tu categoría de proyecto para invertir y registrarse como usuario o empresa.'
+          imgOne={Touken1}
+          imgTwo={Touken2}
+          imgThree={Touken3}
+          imgFour={Touken4}
+          imgFive={Touken5}
+          imgSix={Touken6}
+          imgSeven={Touken7}
+          modalTitle='Imagenes del trabajo'
+        />
+        <Card
+          imgCard={Touken1}
+          title="→ Touken"
+          text='Bienvenido a la web de pre-lanzamiento de StartUp Chile. ¡Nuestra app móvil! En esta plataforma, tanto usuarios como empresas pueden inscribirse para el próximo lanzamiento. Simplemente, elige tu categoría de proyecto para invertir y registrarse como usuario o empresa.'
+          imgOne='https://porfolio-gabriel-cornide.netlify.app/assets/GameCenter-ac9a6fe0.png'
+          imgTwo='https://porfolio-gabriel-cornide.netlify.app/assets/MyTinerary-aea424d2.png'
+          imgThree='https://porfolio-gabriel-cornide.netlify.app/assets/MyPorfolioElectron-b1cbdb98.png'
+          modalTitle='Imagenes del trabajo'
+        />
       </div>
     </>
   );
